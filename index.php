bfffd94fbe7e81b84e37df21e2ff9a4f5ac80aa1
Kaephas <kkain@mail.greenriver.edu>
2019-05-13 16:34:02 -0400

    add survey route and view

--- index.php
+++ index.php
@@ -23,11 +23,28 @@ $f3 = Base::instance();
 //Turn on Fat-Free error reporting
 $f3->set('DEBUG', 3);
 
+// options array
+$f3->set('options', array('This midterm is easy.', 'I like midterms.', 'Today is Monday'));
+
 //Define a default route (dating splash page)
 $f3->route('GET /', function()
 {
     echo '<h1>Midterm Survey</h1>';
-    echo '<a href="/survey">Take My Midterm Survey</a>';
+    echo '<a href="survey">Take My Midterm Survey</a>';
+});
+
+$f3->route('GET|POST /survey', function($f3) {
+    if(!empty($_POST)) {
+        $name = $_POST['name'];
+        $choices = $_POST['options'];
+
+        $f3->set('name', $name);
+        $f3->set('choices', $choices);
+
+    }
+
+    $view = new Template();
+    echo $view->render('views/survey.html');
 });
 
 
